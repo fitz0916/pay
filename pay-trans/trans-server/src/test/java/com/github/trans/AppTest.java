6bd83fbd53b2a6ab5e09196ee042c266fbb89132
谢用培 <xieyongpei@xieyongpeideMacBook-Pro.local>
2020-02-02 17:33:38 +0800

    优化代码

--- pay-trans/trans-server/src/test/java/com/github/trans/AppTest.java
+++ pay-trans/trans-server/src/test/java/com/github/trans/AppTest.java
@@ -25,12 +25,12 @@ public class AppTest {
 	public void _测试支付接口() {
 		PaymentRequest paymentRequest = new PaymentRequest();
 		paymentRequest.setCurrency("CNY");
-		paymentRequest.setCustomerNo("10086122503");
+		paymentRequest.setCustomerNo("10086877891");
 		paymentRequest.setDesc("交易");
 		paymentRequest.setFeature("aa");
 		paymentRequest.setInputCharset("UTF-8");
 		paymentRequest.setNotifyUrl("https://www.baidu.com/");
-		paymentRequest.setPayOrderNo("2020020112283118006");
+		paymentRequest.setPayOrderNo("2020020112283118008");
 		paymentRequest.setSubject("支付宝扫码-支付");
 		paymentRequest.setPayTime("2020-02-27 11:22:11");
 		paymentRequest.setPayAmount("999.06");
@@ -42,7 +42,7 @@ public class AppTest {
 		paymentRequest.setSignType("MD5");
 		paymentRequest.setPayType("0");
 		//paymentRequest.base64Encoder();
-		String sign = PaySignUtil.requestMd5Sign(paymentRequest, "sdadsdsd");
+		String sign = PaySignUtil.requestMd5Sign(paymentRequest, "YrDpbi");
 		paymentRequest.setSign(sign);
 		ModelResult<PaymentResponse>  modelResult = paymentServiceImpl.pay(paymentRequest);
 		
